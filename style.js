35315b153a4ae7f0592c0fc01021794e1608735a
Nick Dumas <dumasn@homes.com>
2021-11-30 12:26:43 -0500

    More cleanup

---
+++ style.js
@@ -0,0 +1,26 @@
+// Colors
+if (card_has_tag(card, "spell")) {
+        card.color = "DarkBlue";
+}
+
+if (card_has_tag(card, "feature")) {
+        card.color = "indigo";
+}
+
+if (card_has_tag(card, "equipment")) {
+        card.color = "FireBrick";
+}
+
+// Icons
+
+if (card_has_tag(card, "soul knife")) {
+        card.icon = "abstract-065";
+}
+
+if (card_has_tag(card, "trickster")) {
+        card.icon = "abstract-003";
+}
+
+if (card_has_tag(card, "equipment")) {
+        card.icon = "trade";
+}
